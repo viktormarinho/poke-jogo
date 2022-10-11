fc363b68085440639c3e6f433868277a47fdec01
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-10-11 11:26:58 -0300

    🐛 FIX: Moving edge function logic out of getServerSideProps

--- pages/game/[pin].tsx
+++ pages/game/[pin].tsx
@@ -5,27 +5,25 @@ import { Pokemon } from "../../common/pokemon";
 import { PageBackground } from "../../components/PageBackground";
 
 type GamePageServerSideProps = {
-    poke: Pokemon
     allPokesList: Pokemon[]
 }
 
-const GamePage = ({ poke, allPokesList }: GamePageServerSideProps) => {
+const GamePage = ({ allPokesList }: GamePageServerSideProps) => {
     return (
         <PageBackground>
-            <GameContainer poke={poke} allPokesList={allPokesList} />
+            <GameContainer allPokesList={allPokesList} />
         </PageBackground>
     )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
     const supabase = getSupabase();
-    const randomPokeId = Math.floor(Math.random() * 906);
+    // const randomPokeId = Math.floor(Math.random() * 906);
 
     const pokes = await supabase.from('pokemons').select('*');
     let allPokesList = pokes.data!;
 
     const props: GamePageServerSideProps = {
-        poke: allPokesList[randomPokeId],
         allPokesList
     }
 
