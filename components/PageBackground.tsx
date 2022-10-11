2f8bf281813cfa2354fe719f7b77e5cfc3be6933
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-10-11 11:24:58 -0300

    📦 NEW: Extract common logic out of components

---
+++ components/PageBackground.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const PageBackground = styled.div`
+  background-color: #b82f43;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
